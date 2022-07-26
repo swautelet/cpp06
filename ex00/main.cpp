31d4d25eee2425b70fcf2cce7d91e311c3b0e9a5
swautele <swautle@student.s19.com>
2022-07-26 13:05:29 +0200

    save ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,26 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		std::cout << "Error number of parameter invalid" << std::endl;
-		return 1;
+		return 0;
 	}
 	std::string param(argv[1]);
-	std::cout << param << std::endl;
+	// std::cout << "I received |" << param << "|" << std::endl;
+	int i = stoi(param);
+	double d = stod(param);
+	float f = stof(param);
+	if (isprint(i))
+	{
+		char c = i;
+		std::cout << "char: " << c << std::endl;
+	}
+	else
+	{
+		std::cout << "char: non displayable" << std::endl; 
+	}
+	std::cout << std::fixed;
+	std::cout << "int: " << i << std::endl;
+	// std::cout << std::precision(std::numeric_limits<d>::digits10;
+	// std::cout << setprecision(std::numeric_limits<double>::max_digits10) << 0.3;
+	std::cout << "double: " << d << std::endl;
+	std::cout << "float: " << f << std::endl;
 }
