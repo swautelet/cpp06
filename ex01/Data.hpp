bd02f615c6e7880e0b81730d5a4a206b1252c36e
swautele <swautle@student.s19.com>
2022-07-26 13:15:34 +0200

    save

---
+++ ex01/Data.hpp
@@ -0,0 +1,16 @@
+#ifndef DATA_HPP
+# define DATA_HPP
+
+class Data{
+	public:
+		Data();
+		~Data();
+		Data(const Data& copi);
+		Data& operator =(const Data& copi);
+
+	private:
+	protected:
+
+}
+
+#endif
